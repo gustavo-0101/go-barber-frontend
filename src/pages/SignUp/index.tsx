f0424454a1c1148ed42beee55976dc28e3f7c1ea
gustavo <guga-rd@hotmail.com>
2021-01-27 11:52:08 -0300

    Página de signin e signup concluídas (com auth).

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -3,7 +3,8 @@ import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import api from '../../services/api';
 
 import getValidationErrors from '../../utils/getValidationErrors';
 
@@ -12,35 +13,67 @@ import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
+import { useToast } from '../../hooks/toast';
+
 import { Container, Content, AnimationContainer, Background } from './styles';
+
+interface SignUpFormData {
+  name: string;
+  email: string;
+  password: string;
+}
 
 const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+  const { addToast } = useToast();
+  const history = useHistory();
 
-  const handleSubmit = useCallback(async (data: object) => {
-    try {
-      formRef.current?.setErrors({});
+  const handleSubmit = useCallback(
+    async (data: SignUpFormData) => {
+      try {
+        formRef.current?.setErrors({});
 
-      const schema = Yup.object().shape({
-        name: Yup.string().required('Precisamos saber o seu nome :)'),
-        email: Yup.string()
-          .required('Aqui vai teu email')
-          .email('Esse email não é válido parcero'),
-        password: Yup.string()
-          .min(8, 'Sua senha precisa ter no mínimo 8 (oito) caracteres')
-          .required('Sem senha = sem acesso :3'),
-      });
-      await schema.validate(data, {
-        abortEarly: false,
-      });
-    } catch (err) {
-      if (err instanceof Yup.ValidationError) {
-        const errors = getValidationErrors(err);
+        const schema = Yup.object().shape({
+          name: Yup.string().required('Precisamos saber o seu nome :)'),
+          email: Yup.string()
+            .required('Aqui vai teu email')
+            .email('Esse email não é válido parcero'),
+          password: Yup.string()
+            .min(8, 'Sua senha precisa ter no mínimo 8 (oito) caracteres')
+            .required('Sem senha = sem acesso :3'),
+        });
 
-        formRef.current?.setErrors(errors);
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        await api.post('/users', data);
+
+        history.push('/');
+
+        addToast({
+          type: 'success',
+          title: 'Cadastro bem sucedido!',
+          description: 'Vocẽ já pode logar :v',
+        });
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+
+          formRef.current?.setErrors(errors);
+
+          return;
+        }
+
+        addToast({
+          type: 'error',
+          title: 'Erro no cadastro parcero :(',
+          description: 'Parece que deu erro no cadastro :3',
+        });
       }
-    }
-  }, []);
+    },
+    [addToast, history],
+  );
 
   return (
     <Container>
